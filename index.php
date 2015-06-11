ffca9cc66d3893f35873369b054b231d10a8e5cd
Ashok <ashok@inbox.com>
2015-06-11 17:55:13 +0530

    New Changes
    Modified with options

--- index.php
+++ index.php
@@ -11,14 +11,15 @@
  Version: 1.0
  Author URI: http://ashokg.in/
  */
- 
+include __DIR__."/functions.php";
 add_action('admin_menu', 'easy_ctc_menu');
+add_action('admin_enqueue_scripts', 'ect_plugin_admin_styles');
 
 function easy_ctc_menu(){
 	add_menu_page( 'Easy Child Theme Creator', 'Easy Child Theme Creator', 'manage_options', 'easy-ctc', 'easy_ctc_menu_init' );
 }
 
 function easy_ctc_menu_init(){
-	include __DIR__."/options.php";
+	include __DIR__ . "/options.php";
 	
 }
